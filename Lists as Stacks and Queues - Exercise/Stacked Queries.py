bcba942076c82a4ae7456f6f8bf3bc0574a65039
Georgi <georgiev1007@icloud.com>
2023-05-18 00:51:15 +0300

    Exercises

---
+++ Lists as Stacks and Queues - Exercise/Stacked Queries.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+numbers = deque()
+
+for i in range(int(input())):
+    number_data = [int(x) for x in input().split()]
+    #map variation for row 6 - map(int, input().split()
+    command = number_data[0]
+
+    if command == 1:
+        numbers.append(number_data[1])
+    elif command == 2:
+        if numbers:
+            numbers.pop()
+    elif command == 3:
+        if numbers:
+            print(max(numbers))
+    elif command == 4:
+        if numbers:
+            print(min(numbers))
+
+numbers.reverse()
+#pop() is an alternative
+print(*numbers, sep=", ")
