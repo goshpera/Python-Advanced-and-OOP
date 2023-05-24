158d7b15f7dece66b7e4a039bcd77da171238c1a
Georgi <georgiev1007@icloud.com>
2023-05-24 15:36:41 +0300

    Lab Exercises

---
+++ Tuples and Sets/05. SoftUni Party.py
@@ -0,0 +1,16 @@
+n = int(input())
+reservations = set()
+
+for _ in range(n):
+    reservation_num = input()
+    reservations.add(reservation_num)
+
+guest_reservation_num = input()
+while guest_reservation_num != "END":
+    if guest_reservation_num in reservations:
+        reservations.remove(guest_reservation_num)
+        guest_reservation_num = input()
+
+print(len(reservations))
+for num in sorted(reservations):
+    print(num)
