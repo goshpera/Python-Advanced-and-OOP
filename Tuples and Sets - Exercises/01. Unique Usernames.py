cbcda2798528566ba7b1e4459e1ab4a568000c85
Georgi <georgiev1007@icloud.com>
2023-05-27 16:17:01 +0300

    Exercises

---
+++ Tuples and Sets - Exercises/01. Unique Usernames.py
@@ -0,0 +1,12 @@
+names_count = int(input())
+names = set()
+
+for _ in range(names_count):
+    names.add(input())
+
+print(*names, sep="\n")
+
+'''
+comprehension method:
+print(*{for _ in range(int(input()))}, sep="\n")
+'''
