881984364348d94eead25af9fd2eee4cb3a65029
Georgi <georgiev1007@icloud.com>
2023-05-31 23:51:21 +0300

    Exercises

--- Stacks, Queues, Tuples, and Sets - Exercise/02. Expression Evaluator.py
+++ Stacks, Queues, Tuples, and Sets - Exercise/02. Expression Evaluator.py
@@ -2,27 +2,27 @@ from collections import deque
 from math import floor
 
 expression = deque(input().split())
-idx = 0
+index = 0
 
-while idx < len(expression):
-    element = expression[idx]
+while index < len(expression):
+    element = expression[index]
 
     if element == "*":
-        for _ in range(idx - 1):
+        for _ in range(index - 1):
             expression.appendleft(int(expression.popleft()) * int(expression.popleft()))
     elif element == "/":
-        for _ in range(idx - 1):
+        for _ in range(index - 1):
             expression.appendleft(int(expression.popleft()) / int(expression.popleft()))
     elif element == "+":
-        for _ in range(idx - 1):
+        for _ in range(index - 1):
             expression.appendleft(int(expression.popleft()) + int(expression.popleft()))
     elif element == "-":
-        for _ in range(idx - 1):
+        for _ in range(index - 1):
             expression.appendleft(int(expression.popleft()) - int(expression.popleft()))
 
     if element in "*/+-":
         del expression[1]
-        idx = 1
-    idx += 1
+        index = 1
+    index += 1
 
 print(floor(int(expression[0])))
