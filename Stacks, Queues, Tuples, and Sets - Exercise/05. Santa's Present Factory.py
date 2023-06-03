cdd036ab46db3d6ab81aef793f3a5b29ab45143a
Georgi <georgiev1007@icloud.com>
2023-06-03 13:02:01 +0300

    Exercises

---
+++ Stacks, Queues, Tuples, and Sets - Exercise/05. Santa's Present Factory.py
@@ -0,0 +1,7 @@
+from collections import deque
+
+toys_materials = deque(int(x) for x in input().split())
+magic_levels = deque(int(x) for x in input().split())
+
+while toys_materials and magic_levels:
+    pass
