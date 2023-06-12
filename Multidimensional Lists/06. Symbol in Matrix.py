6f14b7b130da0645b339a0e64f80eebd34d84e82
Georgi <georgiev1007@icloud.com>
2023-06-12 22:17:25 +0300

    Lab Exercises

--- Multidimensional Lists/06. Symbol in Matrix.py
+++ Multidimensional Lists/06. Symbol in Matrix.py
@@ -3,5 +3,22 @@ rows = int(input())
 matrix = []
 
 for _ in range(rows):
-    inner_list = [el for el in input().split()]
+    inner_list = list(input())  #'abc' turns into ['a', 'b', 'c'] this way
     matrix.append(inner_list)
+
+searched_symbol = input()
+position = None
+
+for row_idx in range(rows):
+    if position:
+        break
+
+    for col_idx in range(rows):
+        if matrix[row_idx][col_idx] == searched_symbol:
+            position = (row_idx, col_idx)
+            break
+
+if position:
+    print(position)
+else:
+    print(f"{searched_symbol} does not occur in the matrix.")
--- Multidimensional Lists/07. Square with Maximum Sum.py
+++ Multidimensional Lists/07. Square with Maximum Sum.py
@@ -1,7 +1,26 @@
-rows, cols = [int(x) for x in input().split()]
+rows, cols = [int(x) for x in input().split(", ")]
 
 matrix = []
+max_sum = float('-inf')
+sub_matrix = []
 
 for _ in range(rows):
-    inner_list = [int(el) for el in input().split()]
+    inner_list = [int(el) for el in input().split(", ")]
     matrix.append(inner_list)
+
+for row_idx in range(rows - 1):
+    for col_idx in range(cols - 1):
+        #We limit the rows and cols loops with 1 less than their values, so we don't get an idx out of range error
+        current_el = matrix[row_idx][col_idx]
+        bellow_el = matrix[row_idx + 1][col_idx]
+        next_el = matrix[row_idx][col_idx + 1]
+        diagonal_el = matrix[row_idx + 1][col_idx + 1]
+        current_sum_elements = current_el + bellow_el + next_el + diagonal_el
+
+        if max_sum < current_sum_elements:
+            max_sum = current_sum_elements
+            sub_matrix = [[current_el, next_el], [bellow_el, diagonal_el]]
+
+print(*sub_matrix[0])
+print(*sub_matrix[1])
+print(max_sum)
