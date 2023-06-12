1d49bb5f18a69799a6982abb37d4c2e58d7b8a8f
Georgi <georgiev1007@icloud.com>
2023-06-12 14:36:01 +0300

    Lab Exercises

--- Multidimensional Lists/06. Symbol in Matrix.py
+++ Multidimensional Lists/06. Symbol in Matrix.py
@@ -3,6 +3,5 @@ rows = int(input())
 matrix = []
 
 for _ in range(rows):
-    inner_list = [int(el) for el in input().split()]
+    inner_list = [el for el in input().split()]
     matrix.append(inner_list)
-
